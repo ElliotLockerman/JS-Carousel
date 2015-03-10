8c97fd9386157f927429a768f2c20babb404b869
Elliot Lockerman <elliot@lockerman.info>
2015-03-10 16:06:59 -0400

    pre-object html preparation

--- src/carousel.js
+++ src/carousel.js
@@ -1,194 +1,306 @@
+
+
+// Checks if a DOM node or any of its children (recursively) has a certain class
+function get_sub_node(root, node_name)
+{
+	
+	if(root.nodeName === node_name) return root;
+	else{
+		for (child in root.childNodes)
+		{
+			if(root.childNodes[child].nodeType === 1) // If its a DOM node
+			{
+				return get_sub_node(root.childNodes[child], node_name)
+			}
+		}
+	}
+	
+	return false;
+}
+
+
+
+
+
 var carousels = [];
 
 function Carousel(reference)
 {	
-	var _this = this;
+	var self = this;
 	
-	_this.carousel_main = reference;
+	self.carousel_main = reference;
 
-
-
-	// Get elements array, number of elements, and element width
-	_this.elements = new Array();
-	for(var child in _this.carousel_main.childNodes)
-	{
-		if(_this.carousel_main.childNodes.hasOwnProperty(child) && 
-			_this.carousel_main.childNodes[child].className === "slider_element")
-		{
-			_this.elements.push(_this.carousel_main.childNodes[child]);
-		}
-	}
-	_this.number_of_elements = _this.elements.length;
-
-
-
-	// Create mask and slider
-	_this.mask = document.createElement("div");
-	_this.mask.setAttribute("class", "carousel_mask");
-	_this.carousel_main.appendChild(_this.mask);
-
-	_this.slider = document.createElement("div");
-	_this.slider.setAttribute("class", "carousel_slider");
-	_this.mask.appendChild(_this.slider);
-	
-	// And add elements to slider
-	for(var element in _this.elements)
-	{
-		_this.slider.appendChild(_this.elements[element]);
-	}
-
-	
-	// Calculate and set element's heights 
-
-	// Get some styles
-	_this.element_width = px_to_int(window.getComputedStyle(_this.elements[0]).width);		
-	_this.slider_styles = window.getComputedStyle(_this.slider);
-	
-	// Overwrite no-js defaults
-	_this.carousel_main.style["overflow-x"] = "auto";
-	
-	_this.slider_position = 0; // Which element's being pointed at. multiply by element_width to get offset, and -1 for movement to left (right button).
-	_this.transition_time = 10; // Default, may be overwritted by config file
-	_this.number_of_frames_per_transition = 40; // Default, may be overwritted by config file
-	_this.frame_delay = this.transition_time / _this.number_of_frames_per_transition;	
-	_this.offset_width = _this.element_width / _this.number_of_frames_per_transition;
-	_this.target_position = 0; // Which element is being, or is intended to be, displayed; changed *before* animation begins; 0 indexed
-
-	
-	
-	
+	self.transition_time = 10; // Default, may be overwritted by config file
+	self.number_of_frames_per_transition = 40; // Default, may be overwritted by config file
 	
 
-	
-	
-	// Buttons and animation
-	
-	// Moves the slider a small amount each time untill it hits the target, if not, recursively repeats
-	_this.slide = function()
-	{		
-		//console.log("Target: " + _this.target_position);
-		
-		var offset;
-		
-		if(px_to_int(_this.slider_styles.left) < _this.target_position)
-		{
-			offset = _this.offset_width;
-		}
-		else
-		{
-			offset = -1 * _this.offset_width;
-		}
-		
-		if(px_to_int(_this.slider_styles.left) != _this.target_position)
-		{
-			//console.log("Position: " + _this.slider_styles.left);
-			
-			_this.slider.style.left = int_to_px(px_to_int(_this.slider_styles.left) + offset);
-			
-			setTimeout(_this.slide, _this.frame_delay);
-		}
-	}
-	
-	
-	_this.left_button_action = function()
-	{		
-		
-		if(_this.slider_position > 0)
-		{
-			_this.target_position = (_this.slider_position * _this.element_width * -1) + _this.element_width;
-			_this.slider_position--;
-			_this.slide();
-		}
-		else if (_this.slider_position == 0)
-		{		
-			_this.target_position = _this.element_width * (_this.number_of_elements - 1) * -1;
-			_this.slider_position = _this.number_of_elements - 1;
-			_this.slide();
-		}
-	};
-	
-	_this.right_button_action = function()
-	{			
-
-		
-		if(_this.slider_position < _this.number_of_elements - 1)
-		{
-			
-			_this.target_position = (_this.slider_position * _this.element_width * -1) - _this.element_width;
-			_this.slider_position++;
-			_this.slide();
-		}
-		else if (_this.slider_position == _this.number_of_elements - 1)
-		{
-			
-			_this.target_position = 0;
-			_this.slider_position = 0;
-			_this.slide();
-		}
-	};
-
-	
-	//create buttons
-	// Left
-	_this.left_button = document.createElement("div");
-	_this.left_button.setAttribute("class", "carousel_button_left");
-	_this.left_button.onclick = _this.left_button_action;
-	
-	_this.left_arrow = document.createElement("span");
-	_this.left_arrow.setAttribute("class", "left_symbol");
-	_this.left_button.appendChild(_this.left_arrow);
-	
-	_this.carousel_main.appendChild(_this.left_button);
-	
-	
-	
-	// Right
-	_this.right_button = document.createElement("div");
-	_this.right_button.setAttribute("class", "carousel_button_right");
-	_this.right_button.onclick = _this.right_button_action;
-
-	_this.right_arrow = document.createElement("span");
-	_this.right_arrow.setAttribute("class", "right_symbol");
-	_this.right_button.appendChild(_this.right_arrow);
-
-	_this.carousel_main.appendChild(_this.right_button);
-	
-	
-	
-	
-	
-	
-	
-	
-
-	// Request the config file; if we get it, load it up
+	// Request the config file and load it up
+	self.config;
 	var xhr = new XMLHttpRequest();
-	xhr.open("GET", document.URL + "/src/config.json", true);
+	xhr.open("GET", document.URL + "src/config.json", true);
 	xhr.onreadystatechange = function(data)
 	{
 	    if (xhr.readyState == 4) 
 		{
-			config = JSON.parse(xhr.responseText);
+			self.config = JSON.parse(xhr.responseText);
+	
 			
-			
-			// Transition time
-			if(typeof config.transition_time !== "undefined") _this.transition_time = config.transition_time;
-
-
-
 			// Number of frames per transition
-			if(typeof config.number_of_frames_per_transition !== "undefined") _this.number_of_frames_per_transition = config.number_of_frames_per_transition;
-					
-					
-		
-		
-			// Re-do some calculations from before, in case the default values were overwritten
-			_this.frame_delay = _this.transition_time / _this.number_of_frames_per_transition;
-			_this.offset_width = _this.element_width / _this.number_of_frames_per_transition;	
+			if(typeof self.config.transition_time === "number") 
+				self.transition_time = self.config.transition_time;
+			
+			// Number of frames per transition
+			if(typeof self.config.number_of_frames_per_transition === "number") 
+				self.number_of_frames_per_transition = self.config.number_of_frames_per_transition;
+			
 		}
 	}
 	xhr.send();
+	
+	
+	// Get all the thumbs
+	self.thumbs = new Array();
+	for (var child in self.carousel_main.childNodes)
+	{	
+		if(self.carousel_main.childNodes[child].nodeType === 1 && 
+			has_sub_node_class(self.carousel_main.childNodes[child], "thumb_image")) // If its a DOM node and it or one of its children has the right class
+		{		
+				self.thumbs.push(self.carousel_main.childNodes[child]);
+		}
+		
+	}
+	
+	
+	
+	// Wrap the thumbs
+	self.thumb_wrapper = document.createElement("div"); 
+	self.thumb_wrapper.setAttribute("class", "thumb_wrapper");
+	self.carousel_main.appendChild(self.thumb_wrapper);
+	
+	for(thumb in self.thumbs)
+	{
+		self.thumb_wrapper.appendChild(self.thumbs[thumb]);
+	}
+	
+	
+	// Create primary wrapper and add imgs
+	self.primary_wrapper = document.createElement("div"); 
+	self.primary_wrapper.setAttribute("class", "primary_wrapper");
+	self.carousel_main.insertBefore(self.primary_wrapper, self.thumb_wrapper);
+	
+	for(thumb in self.thumbs)
+	{
+		link_node = get_sub_node(self.thumbs[thumb], "A")
+		if(link_node)
+		{
+			var new_img = document.createElement("img");
+			new_img.setAttribute("src", link_node.href);
+			new_img.setAttribute("class", "primary_image");
+			self.primary_wrapper.appendChild(new_img);			
+		}
+		else
+		{
+			console.error("Error: The following thumbnail does not have a link to the full-size image", self.thumbs[thumb]);
+		}
+	}
+	
+	
+	
+	
+	
+	 
+	// Create objects which do the rest
+	var sub_carousels
+	self.primary_carousel = new Sub_carousel(self, self.primary_wrapper, "primary_", 1);
+	sub_carousels.push(self.primary_carousel);
+	self.thumb_carousel = new Sub_carousel(self, self.thumb_wrapper, "thumb_", "auto");
+	sub_carousels.push(self.thumb_carousel);
+
+
+
+
+
+
+	function Sub_carousel(outer_object, outer_div, prefix, elements_per_frame)
+	{
+
+		self.slider_position = 0; // Which element's being pointed at. multiply by element_width to get offset, and -1 for movement to left (right button).
+
+
+
+		// Get elements array, number of elements, and element width
+		self.elements = new Array();
+		for(var child in outer_div.childNodes)
+		{
+			if(self.carousel_main.childNodes.hasOwnProperty(child) && 
+				self.carousel_main.childNodes[child].className === prefix + "image")
+			{
+				self.elements.push(self.carousel_main.childNodes[child]);
+			}
+		}
+		self.number_of_elements = self.elements.length;
+
+	
+	
+
+		// Create mask and slider
+		self.mask = document.createElement("div");
+		self.mask.setAttribute("class", prefix + "mask");
+		self.outer_div.appendChild(self.mask);
+
+		self.slider = document.createElement("div");
+		self.slider.setAttribute("class", prefix + "slider");
+		self.outer_div.appendChild(self.slider);
+	
+	
+	
+	
+		// Add images to slide
+		for(var index in self.elements)
+		{
+			self.slider.appendChild(self.elements[index]);
+		}
+
+	
+		// Calculate and set element's heights 
+
+		// Get some styles
+		self.element_width = px_to_int(window.getComputedStyle(self.elements[0]).width);		
+		self.slider_styles = window.getComputedStyle(self.slider);
+	
+		// Overwrite no-js defaults
+		self.carousel_main.style["overflow-x"] = "auto";
+	
+	
+
+		self.frame_delay = this.transition_time / self.number_of_frames_per_transition;	
+		self.offset_width = self.element_width / self.number_of_frames_per_transition;
+		self.target_position = 0; // Which element is being, or is intended to be, displayed; changed *before* animation begins; 0 indexed
+	
+	
+	
+	
+	
+	
+	
+		//create buttons
+		// Left
+		self.left_button = document.createElement("div");
+		self.left_button.setAttribute("class", "carousel_button_left");
+		self.left_button.onclick = self.left_button_action;
+	
+		self.left_arrow = document.createElement("span");
+		self.left_arrow.setAttribute("class", "left_symbol");
+		self.left_button.appendChild(self.left_arrow);
+	
+		self.carousel_main.appendChild(self.left_button);
+	
+	
+	
+		// Right
+		self.right_button = document.createElement("div");
+		self.right_button.setAttribute("class", "carousel_button_right");
+		self.right_button.onclick = self.right_button_action;
+
+		self.right_arrow = document.createElement("span");
+		self.right_arrow.setAttribute("class", "right_symbol");
+		self.right_button.appendChild(self.right_arrow);
+
+		self.carousel_main.appendChild(self.right_button);
+	
+	
+	
+	
+	
+
+	
+	
+	
+	
+	
+	
+		// Functions
+		
+		// Moves the slider a small amount each time untill it hits the target, if not, recursively repeats
+		self.slide = function()
+		{		
+			//console.log("Target: " + self.target_position);
+		
+			var offset;
+		
+			if(px_to_int(self.slider_styles.left) < self.target_position)
+			{
+				offset = self.offset_width;
+			}
+			else
+			{
+				offset = -1 * self.offset_width;
+			}
+		
+			if(px_to_int(self.slider_styles.left) != self.target_position)
+			{
+				//console.log("Position: " + self.slider_styles.left);
+			
+				self.slider.style.left = int_to_px(px_to_int(self.slider_styles.left) + offset);
+			
+				setTimeout(function(){self.slide(slider)}, self.frame_delay);
+			}
+		}
+	
+	
+		self.left_button_action = function()
+		{		
+		
+			if(self.slider_position > 0)
+			{
+				self.target_position = (self.slider_position * self.element_width * -1) + self.element_width;
+				self.slider_position--;
+				self.slide();
+			}
+			else if (self.slider_position == 0)
+			{		
+				self.target_position = self.element_width * (self.number_of_elements - 1) * -1;
+				self.slider_position = self.number_of_elements - 1;
+				self.slide();
+			}
+		};
+	
+		self.right_button_action = function()
+		{			
+			console.log(caller);
+		
+			if(self.slider_position < self.number_of_elements - 1)
+			{
+			
+				self.target_position = (self.slider_position * self.element_width * -1) - self.element_width;
+				self.slider_position++;
+				self.slide();
+			}
+			else if (self.slider_position == self.number_of_elements - 1)
+			{
+			
+				self.target_position = 0;
+				self.slider_position = 0;
+				self.slide();
+			}
+		};
+
+	
+
+	
+	
+	};
+	
+
+	
+	
+	
 };
+
+
+
+
+
 
 
 // Initialize carousels
@@ -209,6 +321,34 @@ window.addEventListener("load", function()
 
 
 
+
+// Checks if a DOM node or any of its children (recursively) has a certain class
+function has_sub_node_class(root, class_name)
+{
+	
+	
+	if(root.className.indexOf(class_name) !== -1) return true;
+	else{
+		for (child in root.childNodes)
+		{
+
+			if(root.childNodes[child].nodeType === 1 && has_sub_node_class(root.childNodes[child], class_name))  // If its a DOM node and it or one of its children has the right class
+			{
+				return true;
+			}
+		}
+	}
+	
+	return false;
+}
+
+
+
+
+
+
+
+
 // Converts css pixel value strings to ints
 function px_to_int(px)
 {
@@ -221,6 +361,9 @@ function px_to_int(px)
 	px.replace("px", "");
     return parseInt(px,10);
 }
+
+
+
 
 
 // Converts ints to css pixel value strings
